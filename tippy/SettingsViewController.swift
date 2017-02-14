850dab23cc2b88876965b82c87c7fd036ec553dc
quayphong <user@users-MacBook-Pro.local>
2017-02-14 11:15:40 +0700

    Initial commit

---
+++ tippy/SettingsViewController.swift
@@ -0,0 +1,81 @@
+//
+//  SettingsViewController.swift
+//  tippy
+//
+//  Created by user on 14/2/17.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+
+    @IBOutlet weak var tipPicker: UIPickerView!
+    
+    let tipPercentages = ["18%", "20%","25%"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int{
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return tipPercentages[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return tipPercentages.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    
+        var currentTip = 0.18
+        switch row {
+        case 1:
+            currentTip = 0.2
+        case 2:
+            currentTip = 0.25
+        default:
+            currentTip = 0.18
+        }
+        
+        let userDefaults = UserDefaults.standard
+        userDefaults.setValue(currentTip, forKey: "DefaultTip")
+        userDefaults.synchronize()
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let userDefault = UserDefaults.standard
+        let defaultTipValue = userDefault.double(forKey: "DefaultTip")
+        var defaultTipIndex = 0
+        
+        switch defaultTipValue {
+        case 0.2:
+            defaultTipIndex = 1
+        case 0.25:
+            defaultTipIndex = 2
+        default:
+            defaultTipIndex = 0
+        }
+        
+        tipPicker.selectRow(defaultTipIndex, inComponent: 0, animated: true)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ tippy/TipViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by user on 14/2/17.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        
+        let tipPercentages = [0.18, 0.2, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let userDefault = UserDefaults.standard
+        let defaultTipValue = userDefault.double(forKey: "DefaultTip")
+        var defaultTipIndex = 0
+        
+        switch defaultTipValue {
+        case 0.2:
+            defaultTipIndex = 1
+        case 0.25:
+            defaultTipIndex = 2
+        default:
+            defaultTipIndex = 0
+        }
+        
+        tipControl.selectedSegmentIndex = defaultTipIndex
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+    }
+}
+
